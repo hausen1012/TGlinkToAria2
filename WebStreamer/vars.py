a7be6eb1b9479af2555c929fc884e711b681f05c
snakexgc <78722169+snakexgc@users.noreply.github.com>
2024-01-06 20:05:37 +0800

    从配置文件中获取aria2的相关配置信息

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -34,3 +34,7 @@ class Var(object):
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
     USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "0").lower()) in ("1", "true", "t", "yes", "y")
     ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "") or "").split(",") if x.strip("@ ")]
+    ARIA2 = bool(environ.get("ARIA2"))
+    RPC_URLS = str(environ.get("API_HASH"))
+    RPC_PORTS = int(environ.get("RPC_PORTS"))
+    RPC_TOKENS = str(environ.get("RPC_TOKENS"))
